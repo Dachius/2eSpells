1d068799ed58c84756f4e6f6bd07c5a15b3ca6d9
Dachius <53961420+Dachius@users.noreply.github.com>
2022-09-05 13:53:40 -0500

    add files by upload

---
+++ js/table.js
@@ -0,0 +1,62 @@
+var _table_ = document.createElement('table'),
+_tr_ = document.createElement('tr'),
+_th_ = document.createElement('th'),
+_td_ = document.createElement('td');
+
+wizardTable();
+
+async function wizardTable(){
+    const response = await fetch('json/Wizard.json')
+    var data = await response.json();
+    realTable = buildHtmlTable(data);
+    realTable.classList.add("sortable");
+    console.log(realTable)
+    document.body.appendChild(realTable);
+    sorttable.makeSortable(realTable);
+}
+
+
+function buildHtmlTable(arr) {
+    var table = _table_.cloneNode(false),
+    columns = addAllColumnHeaders(arr, table);
+    tb = document.createElement('tbody');
+
+    for (var i = 0; i < arr.length; i++) {
+        var tr = _tr_.cloneNode(false);
+        for (var j = 0; j < columns.length; j++) {
+            var td = _td_.cloneNode(false);
+            var cellValue = arr[i][columns[j]];
+            td.appendChild(document.createTextNode(arr[i][columns[j]] || ''));
+            tr.appendChild(td);
+        }
+        tb.appendChild(tr);
+    }
+    table.appendChild(tb);
+    the = document.createElement('thead');
+    the.appendChild(table.rows[0]);
+    table.insertBefore(the,table.firstChild);
+
+    return table;
+}
+
+
+// remove key != stuff for full table
+function addAllColumnHeaders(arr, table) {
+    var columnSet = [],
+    tr = _tr_.cloneNode(false);
+
+    for (var i = 0, l = arr.length; i < l; i++) {
+        for (var key in arr[i]) {
+            if (arr[i].hasOwnProperty(key) && columnSet.indexOf(key) === -1 && (key != "verbal" && key != "somatic" && key != "material")) {
+                columnSet.push(key);
+                var th = _th_.cloneNode(false);
+                th.appendChild(document.createTextNode(key));
+                tr.appendChild(th);
+            }
+        }
+    }
+
+    table.appendChild(tr);
+
+    return columnSet;
+}
